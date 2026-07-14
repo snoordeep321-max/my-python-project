b619b52d18aa5c28369ac5bd237f151c08b3e0b5
Noordeep singh <snoordeep321@gmail.com>
2026-07-14 16:44:15 +0530

    Create System

---
+++ Student-Management-System/main.py
@@ -0,0 +1,19 @@
+while True:
+    print("\n=====Student-Management-System=====")
+    print("1. Add Student")
+    print("2. Display Student")
+    print("3. Exit")
+
+    choice=input("Enter your choice:")
+
+    if choice=="1":
+        print("Adding Student...")
+    elif choice=="2":
+        print("Displaying Student...")
+    elif choice=="3":
+        print("Thank You!")
+        break
+    
+    else:
+        print("Invalid Choice")
+      
